d5a37c0cb872bfee685a8e7ebdcaa595e7e36edc
hicksjc <78804634+hicksjc@users.noreply.github.com>
2021-10-13 16:09:29 -0400

    added contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,29 @@
 import React from 'react';
 import "semantic-ui-css/semantic.css";
 import {Containter, Header, Form, Input, TextArea, Button} from 'semantic-ui-css';
+
+const contactPage = () => {
+    return (
+        <React.Fragment>
+            <Container>
+                <Header>Contact Us!</Header>
+                <Form>
+                    <Form.Field>
+                        <label>First Name</label>
+                        <Input placeholder='Enter your full name...'/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Contact Email:</label>
+                        <Input placeholder='Enter the best email to contact you with'/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Message</label>
+                        <TextArea placeholder='Enter your message...'/>
+                    </Form.Field>
+                    <Button>Send Message</Button>
+                </Form>
+            </Container>
+        </React.Fragment>
+    )
+}
+ export default contactPage;
