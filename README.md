203e23c8a20ccfeafd66a728f6b5384f069f2a8a
hicksjc <78804634+hicksjc@users.noreply.github.com>
2021-10-13 16:48:51 -0400

    added live link

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ James Hicks
 ## Project Name
 
 hcj-midterm
+
+live Link: [hjc-midterm] (https://in-info-web4.informatics.iupui.edu/~hicksjc/N322/hjc-midterm)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~hicksjc/N322/hjc-midterm",
     title: "hjc-midterm",
   },
   plugins: [],
+  pathPrefix: '/~hicksjc/N322/hjc-midterm'
 };
